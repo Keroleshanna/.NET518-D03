7500ecaa24465113f8fcf497a90e6d8a970468e1
Keroles Hanna <keroleshannamatta@gmail.com>
2025-07-16 20:48:48 +0300

    Tasck03 teh Search

---
+++ Search_Task03/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Search_Task03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            /// When should I use a struct instead of a class?????
+            // وهو اصلا حجمة صغير وسريع جدا stack بيتخزن في ال  struct عندي صغيره جدا او بسيط لان ال  Data or application لما تكون ال 
+
+
+
+            /// Recored vs Struct vs Class?????
+            // struct: new فارغ او ان حد يورث منه او يورثه ويستخدم بدون كلمة  constractor ولكنه لا يمكن ان يمتلك  class مثل ال reference  بمعني انه لا يتم تخزينة ك  Value-type هو من نوع 
+            // Class : وغكس كل اللي فوق reference-type  هو من نوع ال 
+            // recoder : يمتاز بانه لا يمكن تغيير قيمة الخصائص بداخلة ونستخدمة لما يكون عندن شوية خصائص فقط بدون تعقيد
+
+        }
+    }
+}
